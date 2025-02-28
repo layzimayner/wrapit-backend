ada91b28eaa49bf929137a0d4129c2905400ce9e
layzimayner <vasyahunta@gmail.com>
2025-02-28 09:52:26 +0200

    checkstyling

--- src/main/java/com/wrap/it/model/Item.java
+++ src/main/java/com/wrap/it/model/Item.java
@@ -1,20 +1,14 @@
 package com.wrap.it.model;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
